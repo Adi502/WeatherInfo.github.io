759d7faf179dd2aff12829d09df69ea8b58418d2
Aditya Soraganvi <adityasoraganvi32@gmail.com>
2022-05-30 17:04:08 +0530

    test commit

--- main/script.js
+++ main/script.js
@@ -33,7 +33,8 @@ let weather = {
       document.querySelector(".weather").classList.remove("loading");
       document.body.style.backgroundImage =
         "url('https://source.unsplash.com/1600x900/?" + name + "')";
-    },
+    
+      },
     search: function () {
       this.fetchWeather(document.querySelector(".search-bar").value);
     },
